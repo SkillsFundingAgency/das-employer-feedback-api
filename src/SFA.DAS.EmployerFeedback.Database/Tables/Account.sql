907fcf14b3b45d38edded47c60aeef4b2ea1e2f3
ratheeshri1984 <ratheeshri1984@gmail.com>
2025-09-09 13:35:37 +0100

    P2-2835-New tables added

---
+++ src/SFA.DAS.EmployerFeedback.Database/Tables/Account.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Account]
+(
+    [Id] BIGINT NOT NULL PRIMARY KEY IDENTITY,
+    [AccountName] NVARCHAR(255) NOT NULL,
+    [CheckedOn] DATETIME2 NULL
+)
+GO
+
+CREATE INDEX [IX_Account_CheckedOn] ON [dbo].[Account] (CheckedOn)
