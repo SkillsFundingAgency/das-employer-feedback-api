ab020440ebb3080e5f4d11ba3723e5ecc11d87d6
Chris Woodcock <chris@humesoftware.com>
2025-09-22 11:04:10 +0100

    Change datatype to bigint to match apprentice feedback

--- src/SFA.DAS.EmployerFeedback.Database/Tables/ProviderAttributeSummary.sql
+++ src/SFA.DAS.EmployerFeedback.Database/Tables/ProviderAttributeSummary.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[ProviderAttributeSummary](
 	[Ukprn] [bigint] NOT NULL,
-	[AttributeId] [int] NOT NULL,
+	[AttributeId] BIGINT NOT NULL,
 	[Strength] [int] NOT NULL,
 	[Weakness] [int] NOT NULL,
 	[TimePeriod] NVARCHAR(50) NOT NULL DEFAULT 'All',
