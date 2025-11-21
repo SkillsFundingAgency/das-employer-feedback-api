b3636d68ad4d8ba31437d1fd4f521ff8b916db84
Alan Burns <adburns57@gmail.com>
2025-11-21 13:59:10 +0000

    Avoid "code duplication" error - by making it different!

--- src/SFA.DAS.EmployerFeedback.Database/StoredProcedures/GenerateProviderAttributeResults.sql
+++ src/SFA.DAS.EmployerFeedback.Database/StoredProcedures/GenerateProviderAttributeResults.sql
@@ -14,19 +14,20 @@ BEGIN
         
     -- Set limit to 5 years
     DECLARE 
-    @limit5AY varchar(6) = 'AY'+RIGHT(YEAR(DATEADD(month,-55,@calcdate)),2)+RIGHT(YEAR(DATEADD(month,-43,@calcdate)),2),
-    @limit1AY varchar(6) = 'AY'+RIGHT(YEAR(DATEADD(month,-7,@calcdate)),2)+RIGHT(YEAR(DATEADD(month,5,@calcdate)),2),
     @timelimit varchar(6),
     @startdate date = CONVERT(date,CONVERT(varchar,YEAR(DATEADD(month,-7,@calcdate)))+'-Aug-01'),
-    @enddate date =   CONVERT(date,CONVERT(varchar,YEAR(DATEADD(month,5,@calcdate)))+'-Aug-01');
+    @enddate date =   CONVERT(date,CONVERT(varchar,YEAR(DATEADD(month,5,@calcdate)))+'-Aug-01'),
+    @limit5AY varchar(6) = 'AY'+RIGHT(YEAR(DATEADD(month,-55,@calcdate)),2)+RIGHT(YEAR(DATEADD(month,-43,@calcdate)),2),
+    @limit1AY varchar(6) = 'AY'+RIGHT(YEAR(DATEADD(month,-7,@calcdate)),2)+RIGHT(YEAR(DATEADD(month,5,@calcdate)),2);
     
-    SET @timelimit = @limit1AY;
     IF @reset = 1
-    -- reset all 5 AYs[dbo].[GenerateProviderRatingResults]
+    -- reset all 5 AYs
     BEGIN
-        SET @startdate = DATEADD(year,-4,@startdate);
         SET @timelimit = @limit5AY;
-    END;
+        SET @startdate = DATEADD(year,-4,@startdate);
+    END
+    ELSE
+        SET @timelimit = @limit1AY;
 
     WITH LatestResults 
     AS (
