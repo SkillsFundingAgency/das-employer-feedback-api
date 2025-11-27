942a6f5d327feabd4d855781f865cf4c009beb0e
ratheeshri1984 <ratheeshri1984@gmail.com>
2025-11-27 13:37:04 +0000

    P2-2824-Revoked the previous commit

--- src/SFA.DAS.EmployerFeedback.Database/Post-Deployment/EmployerFeedbackCleanup.sql
+++ src/SFA.DAS.EmployerFeedback.Database/Post-Deployment/EmployerFeedbackCleanup.sql
@@ -14,5 +14,30 @@ END
 ')
 GO
 
+ -- Drop the default constraint first
+EXEC('
+IF COL_LENGTH(''dbo.EmployerFeedback'', ''IsActive'') IS NOT NULL
+BEGIN
+    -- Find and drop the default constraint on IsActive column
+    DECLARE @DefaultConstraintName NVARCHAR(200)
+    SELECT @DefaultConstraintName = dc.name
+    FROM sys.default_constraints dc
+    INNER JOIN sys.columns c ON dc.parent_column_id = c.column_id AND dc.parent_object_id = c.object_id
+    INNER JOIN sys.objects o ON dc.parent_object_id = o.object_id
+    WHERE o.name = ''EmployerFeedback'' AND c.name = ''IsActive''
+    
+    IF @DefaultConstraintName IS NOT NULL
+    BEGIN
+        DECLARE @DropConstraintSQL NVARCHAR(500) = ''ALTER TABLE [dbo].[EmployerFeedback] DROP CONSTRAINT ['' + @DefaultConstraintName + '']''
+        EXEC sp_executesql @DropConstraintSQL
+    END
+END
+')
+GO
+
+-- Drop IsActive column if it exists
+EXEC('IF COL_LENGTH(''dbo.EmployerFeedback'', ''IsActive'') IS NOT NULL ALTER TABLE [dbo].[EmployerFeedback] DROP COLUMN [IsActive]')
+GO
+
 PRINT 'EmployerFeedback Cleanup: Completed cleanup operations'
 GO
